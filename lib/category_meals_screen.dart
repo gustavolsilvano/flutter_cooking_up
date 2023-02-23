cc4dbfecca6cd6573830cfc4bc1c8fc88f3ecc83
Gustavo Silvano <gustavolsilvano@gmail.com>
2023-02-23 17:49:23 -0300

    feat: creating Meal model

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -17,8 +17,6 @@ class CategoryMealsScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text(categoryTitle != null ? categoryTitle : ''),
         ),
-        body: Center(
-          child: Text('The Recipes For The Category!'),
-        ));
+        body: ListView.builder(itemBuilder: (_,index)=> ,itemCount: ,),);
   }
 }
